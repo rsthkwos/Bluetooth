9090a32770c179c478c3d1ae127352994fd023ed
RST <rsthkwos@gmail.com>
2015-06-02 22:37:15 +0800

    bluetooth beta 2.0

--- app/src/main/java/com/rd2/tw/bluetooth/MainActivity.java
+++ app/src/main/java/com/rd2/tw/bluetooth/MainActivity.java
@@ -2,8 +2,12 @@ package com.rd2.tw.bluetooth;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,10 +16,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
 
     @Override
@@ -54,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
     private static OutputStream mOutputStream = null;
     private final int REQUEST_ENABLE_BT = 1;
 
+    private ArrayList<String> mArrayAdapter;
+    private BroadcastReceiver mReceiver;
 
     public void buttonOnClick(View v) {
 
@@ -69,6 +80,54 @@ public class MainActivity extends AppCompatActivity {
             Intent mIntentOpenBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(mIntentOpenBT, REQUEST_ENABLE_BT);
         }
+
+        //取得目前已經配對過的裝置
+        Set<BluetoothDevice> setPairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        //如果已經有配對過的裝置
+        if (setPairedDevices.size() > 0) {
+            // 把裝置名稱以及MAC Address印出來
+            for (BluetoothDevice device : setPairedDevices) {
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+
+        //註冊一個BroadcastReceiver，主要是用來接收【搜尋到附近藍芽裝置】的訊息
+        // 註冊一個 BroadcastReceiver，等等會用來接收搜尋到裝置的消息
+        IntentFilter filter = new IntentFilter((BluetoothDevice.ACTION_FOUND));
+        registerReceiver(mReceiver, filter);
+        mBluetoothAdapter.startDiscovery(); //開始搜尋裝置
+
+        //宣告上段code使用到的BroadcastReceiver
+        final BroadcastReceiver mReceiver = new BroadcastReceiver(){
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //當收尋到裝置時
+                if(BluetoothDevice.ACTION_FOUND.equals(intent.getAction())){
+                    //取得藍芽裝置這個物件
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                    //判斷那個裝置是不是你要連結的裝置，根據藍芽裝置名稱判斷
+                    if(device.getName().equals("MY_DEVICE_ID")){
+                        try{
+                            //一進來一定要停止搜尋
+                            mBluetoothAdapter.cancelDiscovery();
+                            //連結到該裝置
+                            mBluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(MY_UUID);
+                            mBluetoothSocket.connect();
+                            //取得outputstream
+                            mOutputStream = mBluetoothSocket.getOutputStream();
+                            //送出訊息
+                            String message = "hello";
+                            mOutputStream.write(message.getBytes());
+                        }catch(IOException e){
+
+                        }
+                    }
+                }
+            }
+        };
 
         /*
         Button btn = (Button)v;
